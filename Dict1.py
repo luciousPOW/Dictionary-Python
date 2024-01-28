9b2d0d7e26302733fc5ddfc0ecab43328f4520ed
yozora <128562373+luciousPow@users.noreply.github.com>
2024-01-28 12:19:07 +0800

    Add files via upload

---
+++ Dict1.py
@@ -0,0 +1,8 @@
+students = {
+    "Elon": "Musk", 
+    "Harry": "Styles", 
+    "Bill": "Gates",
+}
+
+for student in students:
+    print(student, students[student], sep=": ")
---
+++ Dict2.py
@@ -0,0 +1,8 @@
+students = [
+    {"name": "Rey", "House": "Anon", "patronus": "Otter"}, 
+    {"name": "Jun", "House": "Bayan", "patronus": "Autor"},
+    {"name": "Randy", "House": "Qc", "patronus": "Wesley"},
+]
+
+for student in students:
+    print(student["name"], student["House"], student["patronus"], sep=", ")
